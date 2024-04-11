40f143012f1a737938b3d691d81eb3786c536df6
247731 Marszałek Piotr <247731@edu.p.lodz.pl>
2024-04-11 10:33:08 +0200

    Model API

--- Logic/LogicAPI.cs
+++ Logic/LogicAPI.cs
@@ -1,4 +1,6 @@
 ﻿
+using Data;
+
 namespace Logic
 {
     public abstract class LogicAPI
@@ -6,6 +8,11 @@ namespace Logic
         public abstract void CreateBalls(int amount);
         public abstract void RunSimulation();
         public abstract void StopSimulation();
+        public abstract List<IBall> Balls { get; }
 
+        public static LogicAPI CreateInstance()
+        {
+            return new BallManager();
+        }
     }
 }
