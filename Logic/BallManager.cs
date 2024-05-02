8450bf225e0744aaced0759c261f2ea4c73e56e9
247686 Kaczmarek Jakub <247686@edu.p.lodz.pl>
2024-05-02 21:41:47 +0200

    collisions almost working

--- Logic/BallManager.cs
+++ Logic/BallManager.cs
@@ -1,4 +1,6 @@
 ﻿using Data.Abstract;
+using System.Diagnostics;
+using System.Runtime.Intrinsics;
 namespace Logic;
 internal class BallManager : LogicAPI
 {
@@ -53,6 +55,7 @@ internal class BallManager : LogicAPI
         IBall Ball = (IBall)sender;
         (float x, float y) = Ball.getPosition();
         (float xSpeed, float ySpeed) = Ball.getSpeed();
+
         if (!WithinBoundariesOnAxis(x, Data.GetBallRadius(), Data.GetTableWidth()))
         {
             xSpeed = -xSpeed;
@@ -66,22 +69,42 @@ internal class BallManager : LogicAPI
         {
             if (OtherBall != Ball && IsColliding(Ball, OtherBall))
             {
-                
+ 
                 //function for calculating new velocity
                 ElasticCollision(Ball, OtherBall);
+                return;
             }
         }
         Ball.ChangeSpeed(xSpeed, ySpeed);
     }
     private void ElasticCollision(IBall ball1, IBall ball2)
     {
+        //System.Diagnostics.Debug.WriteLine("Collision with another ball");
+        (float x1, float y1) = ball1.getPosition();
         (float xSpeed1, float ySpeed1) = ball1.getSpeed();
+        (float x2, float y2) = ball2.getPosition();
         (float xSpeed2, float ySpeed2) = ball2.getSpeed();
+        (float x, float y) newSpeedBall1, newSpeedBall2,
+                           v1_v2 = (xSpeed1 - xSpeed2, ySpeed1 - ySpeed2),
+                           v2_v1 = (xSpeed2 - xSpeed1, ySpeed2 - ySpeed1),
+                           x1_x2 = (x1 - x2, y1 - y2),
+                           x2_x1 = (x2 - x1, y2 - y1);
+        float dist = EuclideanDistance(ball1, ball2);
+        float sqDist = dist * dist;
+        float coef1, coef2;
+        coef1 = (v1_v2.x * x1_x2.x + v1_v2.y * x1_x2.y) / sqDist;
+        coef2 = (v2_v1.x * x2_x1.x + v2_v1.y * x2_x1.y) / sqDist;
+        newSpeedBall1 = (xSpeed1 - x1_x2.x * coef1, ySpeed1 - x1_x2.y * coef1);
+        newSpeedBall2 = (xSpeed2 - x2_x1.x * coef2, ySpeed2 - x2_x1.y * coef2);
+        ball1.ChangeSpeed(newSpeedBall1.x, newSpeedBall1.y);
+        ball2.ChangeSpeed(newSpeedBall2.x, newSpeedBall2.y);
 
-        throw new NotImplementedException();
+
+
     }
     private bool IsColliding(IBall ball1, IBall ball2)
     {
+        
         float radius = Data.GetBallRadius();
         if (EuclideanDistance(ball1, ball2) <= 2 * radius)
         {
@@ -93,7 +116,7 @@ internal class BallManager : LogicAPI
     {
         (float x1, float y1) = ball1.getPosition();
         (float x2, float y2) = ball2.getPosition();
-        return (float)Math.Sqrt(x1 * x1 + y1 * y2);
+        return (float)Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 
     }
     public override void StopSimulation()
