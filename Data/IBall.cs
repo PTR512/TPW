390e7841ee9283582972c09c1581f8cbcba91a8f
247686 Kaczmarek Jakub <247686@edu.p.lodz.pl>
2024-06-01 17:56:25 +0200

    IBall changed

--- Data/IBall.cs
+++ Data/IBall.cs
@@ -8,17 +8,10 @@ namespace Data.Abstract
         public abstract void ChangeSpeed(Vector2 speed);
         public abstract void StopBall();
         public abstract void LetBallMove();
-        // provides X, Y, speedX, speedY
-        public abstract Vector2 getSpeed();
-        public static IBall CreateInstance(Vector2 position, Vector2 speed, bool isRunning)
-        // provides X, Y, speedX, speedY
-        public abstract Vector2 getSpeed();
-        // provides X, Y, speedX, speedY
-        public abstract Vector2 getSpeed();
+
         // provides X, Y, speedX, speedY
         public abstract Vector4 getPositionAndSpeed();
-        public abstract float getMass();
-        public static IBall CreateInstance(Vector2 position, float radius, Vector2 speed, bool isRunning, float mass)
+        public static IBall CreateInstance(Vector2 position, Vector2 speed, bool isRunning)
         {
             return new Ball(position, speed, isRunning);
         }
