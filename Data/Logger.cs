d6d8cb679851d1d1702fda520b03eb189659dca3
247731 Marszałek Piotr <247731@edu.p.lodz.pl>
2024-06-11 19:01:47 +0200

    logger small changes

--- Data/Logger.cs
+++ Data/Logger.cs
@@ -7,72 +7,82 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Threading.Tasks.Dataflow;
+
 namespace Data
 {
     internal record BallInfoRecord(DateTime timestamp, int id, float x, float y, float xSpeed, float ySpeed);
+
     internal class Logger : IDisposable
     {
         private BlockingCollection<BallInfoRecord> blockingCollection;
-        private StreamWriter file;
         private bool isDisposed = false;
-        static private Logger logger = null;
+        private static Logger logger = null;
+        private static Thread _worker = null;
+
         private Logger()
         {
             blockingCollection = new BlockingCollection<BallInfoRecord>(15);
-            file = new StreamWriter($"{Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName}/logs.json", append: true, Encoding.UTF8);
         }
-        
 
-        static public Logger getLogger()
+        public static Logger getLogger()
         {
             if (logger == null)
             {
                 logger = new Logger();
-                new Thread(new ThreadStart(logger.saveLogToFile)).Start();
-                
-
+                _worker = new Thread(new ThreadStart(logger.saveLogToFile));
+                _worker.Start();
             }
             return logger;
         }
+
         public void logBallInfo(int id, Vector4 ballInfo, DateTime timestamp)
         {
             BallInfoRecord info = new(timestamp, id, ballInfo[0], ballInfo[1], ballInfo[2], ballInfo[3]);
             blockingCollection.TryAdd(info);
-
         }
+
         private async void saveLogToFile()
         {
-       
-                try
-                {
+
+                using (StreamWriter file = new StreamWriter($"{Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName}/logs.json", append: true, Encoding.UTF8))
+                { 
                     foreach (var item in blockingCollection.GetConsumingEnumerable())
                     {
-                    string ballInfoString = JsonSerializer.Serialize(item);
-                    file.Write(ballInfoString);
+                        string ballInfoString = JsonSerializer.Serialize(item);
+                        await file.WriteLineAsync(ballInfoString);
                     }
                 }
-                catch (Exception e)
-                {
-
-                }
-                
-            
-            file.Close();
-
         }
-        void IDisposable.Dispose()
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
         {
             if (!isDisposed)
             {
-                blockingCollection.CompleteAdding();
-                // tutaj thread.join() i thread.dispose
-                blockingCollection.Dispose();
-                file.Flush ();  // chyba
-                file.Dispose();
+                if (disposing)
+                {
+                    blockingCollection.CompleteAdding();
+                    if (_worker != null)
+                    {
+                        _worker.Join();
+                        _worker = null;
+                    }
+                    blockingCollection.Dispose();
+                }
                 isDisposed = true;
             }
+        }
+
+        ~Logger()
+        {
+            Dispose(false);
         }
     }
 }
